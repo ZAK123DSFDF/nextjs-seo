c8fdd08707b41ac97a31a841050f145bebad74f0
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2024-03-22 20:14:40 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "this is layout app",
+  description: "this is layout app description",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div>this is shared component</div>
+        {children}
+      </body>
+    </html>
+  );
+}
